013f3e643486b6de47e2f5547c11534fb8525a20
Gururaj <gkulkarni@zynga.com>
2019-04-18 15:58:47 +0530

    Adds a few python scrapped dataset

--- scrapping_code.py
+++ scrapping_code.py
@@ -4,13 +4,15 @@ import time
 import sys
 
 driver = webdriver.Chrome('/Users/gkulkarni/Applications/chromedriver/2.45/chromedriver')
+lang_code = {"cpp": 44, "c": 11, "py": 116}
 
 contest_name = sys.argv[1]
 question_name = sys.argv[2]
 language = sys.argv[3]
+lcode= lang_code[language]
 pages = sys.argv[4] if len(sys.argv)>4 else 1
 
-driver.get('https://www.codechef.com/%s/status/%s?sort_by=All&sorting_order=asc&language=%s&status=15&handle=&Submit=GO' %(contest_name, question_name, "44"))
+driver.get('https://www.codechef.com/%s/status/%s?sort_by=All&sorting_order=asc&language=%s&status=15&handle=&Submit=GO' %(contest_name, question_name, lcode))
 
 path = "dataset/scrapped_codes/" + contest_name + '-' + question_name
 os.system("mkdir -p " + path)
